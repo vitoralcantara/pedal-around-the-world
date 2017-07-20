42e498291f9f92e2a56a85f271ad1281414f098d
Vítor Alcântara de Almeida <vit.alcantara@gmail.com>
2017-07-20 14:24:49 -0300

    Adicionado coluna com status do usuário.

--- gratuidade/admin.py
+++ gratuidade/admin.py
@@ -8,30 +8,11 @@ from djtools.utils import rtr, httprr
 #admin.site.register(PessoaGratuidade)
 
 class PessoaGratuidadeAdmin(admin.ModelAdmin):
-    
-#    def get_urls(self):
-#        from django.conf.urls.defaults import patterns
-#        urls = super(PessoaGratuidadeAdmin, self).get_urls()
-#        my_urls = patterns('', 
-#                           (r'^(.+)/add/$', self.gratuidade_add),                           
-#                           )
-#        return my_urls + urls
-#    
-#    @rtr()
-#    def gratuidade_add(self, request, object_id):
-#        if request.method == 'POST':
-#            form = PessoaGratuidadeForm(request.POST)
-#            if form.is_valid():
-#                form.save()
-#                return httprr('', u'Inscrição realizada com sucesso!')
-#        else:
-#            form = PessoaGratuidadeForm()
-#        return locals()
-    
+
     form = PessoaGratuidadeForm
     
     search_fields = ['nome', 'numero_carteira', 'cpf', 'carteira_antigo']
-    list_display = ['editar', 'carteira', 'nome', 'cpf', 'numero_carteira', 'validade', 'tipagem']
+    list_display = ['editar', 'carteira','situacao', 'nome', 'cpf', 'numero_carteira', 'validade', 'tipagem']
     
     fieldsets = [
         ('Dados Pessoais', {'fields': ['nome', 'cpf','data_nascimento','telefone','rg', 'via_documento', 'uf','naturalidade','estado_civil','nacionalidade','nome_mae','nome_pai','email']}),
@@ -42,8 +23,8 @@ class PessoaGratuidadeAdmin(admin.ModelAdmin):
     def editar(self, obj):
         return '<img src="/media/img/16x16/editar.png" title="Editar" />'
     editar.allow_tags = True
-    editar.short_description = ''
-    editar.attrs = {'width': '10px'}
+    editar.short_description = 'Editar'
+    #editar.attrs = {'width': '1px'}
 
     def tipagem(self, obj):
         if obj.tipo == 'tratmedico':
@@ -58,11 +39,12 @@ class PessoaGratuidadeAdmin(admin.ModelAdmin):
     tipagem.allow_tags = True
     tipagem.short_description = 'Tipo de Pessoa'
 
-    def carteira(self, obj):        
-        return '<a href="/gratuidade/pessoagratuidade/cartao_gratuidade/%s/"><img src="/media/img/16x16/cartao_yes.png" title="Imprimir Cartão" /></a>' % (obj.pk)        
+    def carteira(self, obj):
+        return '<a href="/gratuidade/pessoagratuidade/cartao_gratuidade/%s/"><img src="/media/img/16x16/cartao_yes.png" title="Imprimir Cartão" /></a>' % (
+        obj.pk)
+
     carteira.allow_tags = True
     carteira.short_description = 'Cartão'
-    carteira.attrs = {'width': '10px'}
 
     
 admin.site.register(PessoaGratuidade, PessoaGratuidadeAdmin)
